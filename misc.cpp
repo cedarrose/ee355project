5e5edf23efdc129e4fc3748e2d3dfe4a4e6c77ed
katherinezho <76193489+katherinezho@users.noreply.github.com>
2025-05-05 20:00:37 -0700

    part 2 and 3

--- misc.cpp
+++ misc.cpp
@@ -14,3 +14,10 @@ void printMe(string type){
         cout << endl << banner << endl << endl;
     }
 }
+
+string codeName(string str1, string str2){
+    string result = str1 + str2;
+    result.erase(remove_if(result.begin(), result.end(), ::isspace), result.end());
+    transform(result.begin(), result.end(), result.begin(), ::tolower);
+    return result;
+}
